7edd9abf23c0f47d9bcd1b152638b9c9c0e21ac1
vermastha <74677604+vermastha@users.noreply.github.com>
2021-09-05 11:16:49 +0530

    Add files via upload

---
+++ externalapi.js
@@ -0,0 +1,10 @@
+//https://www.npmjs.com/package/superheroes
+
+// npm i superheroes or npm install superheroes, then go to package.json
+//and check for a new dependency called superheroes.
+
+
+const superheroes = require('superheroes');
+
+console.log(superheroes.random());
+
